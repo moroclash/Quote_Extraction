b6e1783184764459cfe0e9879c215399642d55cd
Umar Mohamed <umar.m.ibrahime@outlook.com>
2022-12-26 16:44:16 +0200

    add Polnear Dataset Handler & update RiQuA Dataset Handler

--- scripts/convert.py
+++ scripts/convert.py
@@ -22,6 +22,12 @@ NAMES_MAPPER = {"Cue": "CUE",
                 "RightSpeaker": "QUOTE",
                 "Unknown": "QUOTE",
                 "Speaker": "SPEAKER",
+                "B-source": "B-SPEAKER",
+                "I-source": "I-SPEAKER",
+                "B-cue": "B-CUE",
+                "I-cue": "I-CUE",
+                "B-content": "B-QUOTE",
+                "I-content": "I-QUOTE",
                 }
 
 
@@ -105,42 +111,93 @@ def convert_DirectQuote_to_docs(
 
 
 
+# def convert_RiQuA_to_docs(
+#     folder_path: str,
+# ) -> List[Doc]:
+#     """Parse RiQuA dataset into spaCy docs
+#     """
+
+#     nlp = spacy.blank("en")
+#     all_docs = []
+#     all_entities = []
+
+#     for file_ in filter(lambda x: x[-3:] == 'txt' ,os.listdir(folder_path)):
+#         # read content of each file
+#         with open(f'{folder_path}/{file_}', 'r') as f:
+#             content = f.readlines()[0]
+    
+#         doc = nlp.make_doc(content)
+
+#         with open(f'{folder_path}/{file_[:-4]}.ann', 'r') as f:
+#             annotations = f.readlines()
+    
+#         entities = list(map(lambda x: x.split('\t')[1].split(' '), filter(lambda x: x[0] == 'T' , annotations)))
+
+#         all_entities += list(filter(lambda x: x[0] == 'T' , annotations))
+    
+#         spans = []
+#         for tag, start, end in entities:
+#             tag = NAMES_MAPPER[tag]
+#             span = doc.char_span(int(start), int(end), label=tag, alignment_mode='expand')
+#             if span != None:
+#                 spans.append(Span(doc, span.start, span.end, label=tag))
+#         doc.spans["sc"] = spans
+#         all_docs.append(doc)
+
+#     Cues_verbs = set(i[0].lower() for i in filter(lambda x: x[1] == 'Cue', map(lambda x: (x.split('\t')[2].replace('\n', ''), x.split('\t')[1].split()[0]), all_entities)))
+
+#     return all_docs, Cues_verbs
+
 def convert_RiQuA_to_docs(
     folder_path: str,
-) -> List[Doc]:
+) -> List[Doc], List[str]:
     """Parse RiQuA dataset into spaCy docs
     """
 
+    with open(file_path, 'r') as f:
+        data_ = f.readlines()
+    
     nlp = spacy.blank("en")
-    all_docs = []
-    all_entities = []
-
-    for file_ in filter(lambda x: x[-3:] == 'txt' ,os.listdir(folder_path)):
-        # read content of each file
-        with open(f'{folder_path}/{file_}', 'r') as f:
-            content = f.readlines()[0]
-    
-        doc = nlp.make_doc(content)
-
-        with open(f'{folder_path}/{file_[:-4]}.ann', 'r') as f:
-            annotations = f.readlines()
-    
-        entities = list(map(lambda x: x.split('\t')[1].split(' '), filter(lambda x: x[0] == 'T' , annotations)))
-
-        all_entities += list(filter(lambda x: x[0] == 'T' , annotations))
-    
-        spans = []
-        for tag, start, end in entities:
-            tag = NAMES_MAPPER[tag]
-            span = doc.char_span(int(start), int(end), label=tag, alignment_mode='expand')
-            if span != None:
-                spans.append(Span(doc, span.start, span.end, label=tag))
+    docs = []
+    Cues_verbs = []
+    for row in data_:
+        row = eval(row)
+        tokens, tags = row['tokens'], row['labels']
+        tags = [NAMES_MAPPER[t] if t in NAMES_MAPPER else t for t in tags]
+        spaces = [True]*len(tokens)
+        doc = Doc(nlp.vocab, words=tokens, spaces=spaces, ents=tags)
+        spans = [Span(doc, ent.start, ent.end, label=ent.label_) for ent in doc.ents]
         doc.spans["sc"] = spans
-        all_docs.append(doc)
+        docs.append(doc)
+        Cues_verbs += map(lambda x: x.text.lower(), filter(lambda x: x.label_ == 'CUE' ,doc.ents))
+    return docs, Cues_verbs
 
-    Cues_verbs = set(i[0].lower() for i in filter(lambda x: x[1] == 'Cue', map(lambda x: (x.split('\t')[2].replace('\n', ''), x.split('\t')[1].split()[0]), all_entities)))
 
-    return all_docs, Cues_verbs
+
+
+def convert_Polnear_to_docs(
+    folder_path: str,
+) -> List[Doc], List[str]:
+    """Parse Polnear dataset into spaCy docs
+    """
+
+    with open(file_path, 'r') as f:
+        data_ = f.readlines()
+    
+    nlp = spacy.blank("en")
+    docs = []
+    Cues_verbs = []
+    for row in data_:
+        row = eval(row)
+        tokens, tags = row['tokens'], row['labels']
+        tags = [NAMES_MAPPER[t] if t in NAMES_MAPPER else t for t in tags]
+        spaces = [True]*len(tokens)
+        doc = Doc(nlp.vocab, words=tokens, spaces=spaces, ents=tags)
+        spans = [Span(doc, ent.start, ent.end, label=ent.label_) for ent in doc.ents]
+        doc.spans["sc"] = spans
+        docs.append(doc)
+        Cues_verbs += map(lambda x: x.text.lower(), filter(lambda x: x.label_ == 'CUE' ,doc.ents))
+    return docs, Cues_verbs
 
 
 def Train_Test_Split(all_docs:List[Doc], split_ratio:float=.3):
@@ -163,33 +220,66 @@ def main(
     test_ratio: float = typer.Option(..., "--split-ratio", exists=True),
     output_path: Path = typer.Option(..., "-o", "--output", exists=True),
 ):
-    Cue_verbs = []
+    all_Cue_verbs = []
     all_docs = []
 
     with msg.loading(f"Processing RiQuA Dataset..."):
-        docs_RiQuA, Cue_verbs = convert_RiQuA_to_docs('assets/riqua/merged')
+        len_docs_RiQuA = 0
+        docs_RiQuA, Cue_verbs = convert_RiQuA_to_docs('assets/riqua_train.txt')
+        len_docs_RiQuA += len(docs_RiQuA)
         all_docs += docs_RiQuA
-        msg.good(f"Processing Done :: {len(docs_RiQuA)} Docs")
+        all_Cue_verbs += Cue_verbs 
+
+        docs_RiQuA, Cue_verbs = convert_RiQuA_to_docs('assets/riqua_test.txt')
+        len_docs_RiQuA += len(docs_RiQuA)
+        all_docs += docs_RiQuA
+        all_Cue_verbs += Cue_verbs 
+
+        docs_RiQuA, Cue_verbs = convert_RiQuA_to_docs('assets/riqua_valid.txt')
+        len_docs_RiQuA += len(docs_RiQuA)
+        all_docs += docs_RiQuA
+        all_Cue_verbs += Cue_verbs 
+    msg.good(f"Processing Done :: {len_docs_RiQuA} Docs")
+
+
+    with msg.loading(f"Processing Polnear Dataset..."):
+        len_docs_polnear = 0
+        docs_polnear, Cue_verbs = convert_Polnear_to_docs('assets/polnear_train.txt')
+        len_docs_Polnear += len(docs_polnear)
+        all_docs += docs_polnear
+        all_Cue_verbs += Cue_verbs 
+
+        docs_polnear, Cue_verbs = convert_Polnear_to_docs('assets/polnear_test.txt')
+        len_docs_Polnear += len(docs_polnear)
+        all_docs += docs_polnear
+        all_Cue_verbs += Cue_verbs 
+
+        docs_polnear, Cue_verbs = convert_Polnear_to_docs('assets/polnear_valid.txt')
+        len_docs_Polnear += len(docs_polnear)
+        all_docs += docs_polnear
+        all_Cue_verbs += Cue_verbs 
+    msg.good(f"Processing Done :: {len_docs_polnear} Docs")
+
 
     with msg.loading(f"Processing DirectQuote Dataset..."):
         with open('assets/DirectQuote/truecased.txt', "r", encoding="utf-8") as f:
             data = f.readlines()
-        docs_DirectQuote = convert_DirectQuote_to_docs(data, Cue_verbs)
+        docs_DirectQuote = convert_DirectQuote_to_docs(data, set(all_Cue_verbs))
         all_docs += docs_DirectQuote
     msg.good(f"Processing Done :: {len(docs_DirectQuote)} Docs")
 
     with msg.loading(f"Train Test Split for :: {len(all_docs)} Docs  with test Ration {test_ratio}...."):
         train_set, test_set = Train_Test_Split(all_docs, float(test_ratio))
-        msg.good(f"Spliting Done")
+    msg.good(f"Spliting Done")
 
     with msg.loading(f"Saving into DocBin..."):
         doc_bin = DocBin(docs=train_set)
         doc_bin.to_disk(f"{output_path}/train.spacy")
-        msg.good(f"Saved to {output_path}/train.spacy")
+    msg.good(f"Saved to {output_path}/train.spacy")
 
         doc_bin = DocBin(docs=test_set)
         doc_bin.to_disk(f"{output_path}/test.spacy")
-        msg.good(f"Saved to {output_path}/test.spacy")
+    msg.good(f"Saved to {output_path}/test.spacy")
 
 
 if __name__ == "__main__":
